e6b7bdecf83c18267c6f0a3f7644e9f286483e72
Bojana Sofronovic <sofronovicn@gmail.com>
2018-09-13 22:17:09 +0200

    Student model class updated

--- src/main/java/sservice/student/service/model/Student.java
+++ src/main/java/sservice/student/service/model/Student.java
@@ -1,9 +1,17 @@
 package sservice.student.service.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
@@ -16,12 +24,38 @@ public class Student extends User {
 	
 	@Column(unique = true, nullable = false)
 	private String index;
-
 	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+	@JoinColumn(name = "studyProgram_id", referencedColumnName = "studyProgram_id")
+	private StudyProgram studyProgram;
 	
+	@OneToMany(mappedBy = "student", fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+	private List<Document> documentList = new ArrayList<Document>();
+	
+	@OneToMany(mappedBy = "student", fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+	private List<Exam> examList = new ArrayList<Exam>();
+	
+	@OneToMany(mappedBy = "student", fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+	private List<Payment> paymentList = new ArrayList<Payment>();
+	
+	@OneToMany(mappedBy = "student", fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+	private List<CourseAttending> courseList = new ArrayList<CourseAttending>();
 	
 	public Student() {}
 	
+	public Student(long id, String username, String firstname, String lastname, String birthday, String email,
+			String index, StudyProgram studyProgram, List<Document> documentList, List<Exam> examList,
+			List<Payment> paymentList, List<CourseAttending> courseList) {
+		super(id, username, firstname, lastname, birthday, email);
+		this.id = id;
+		this.index = index;
+		this.studyProgram = studyProgram;
+		this.documentList = documentList;
+		this.examList = examList;
+		this.courseList = courseList;
+		this.paymentList = paymentList;
+	}
+
 	public long getId() {
 		return id;
 	}
@@ -37,6 +71,5 @@ public class Student extends User {
 	public void setIndex(String index) {
 		this.index = index;
 	}
-	
 	
 }
